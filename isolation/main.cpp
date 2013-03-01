88178b4bfc03dffcd35df704d9d3d454923e9ca3
Kevin Graney <nanonet@gmail.com>
2013-02-28 23:37:31 -0500

    Initial Commit

---
+++ isolation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  isolation
+//
+//  Created by Kevin Graney on 2/28/13.
+//  Copyright (c) 2013 Kevin Graney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
